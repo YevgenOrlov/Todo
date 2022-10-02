d5b6423bba8835fe844fbfdf2789cf5c0ffd4c76
YevgenOrlov <101797140+YevgenOrlov@users.noreply.github.com>
2022-10-02 22:24:21 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import Header from "./components/Header.js";
+import Form from "./components/Form.js";
+import "./App.css";
+import TodosList from "./components/TodosList";
+
+const App = () => {
+
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([]);
+  return (
+    <div className="container">
+      <div className="app-wrapper">
+        <div>
+          <Header />
+        </div>
+        <div>
+          <Form 
+           input={input}
+           setInput={setInput}
+           todos={todos}
+           setTodos={setTodos}
+          />
+        </div>
+        <div>
+        <TodosList todos={todos} setTodos={setTodos} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
